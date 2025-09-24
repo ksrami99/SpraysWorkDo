c9c87ee46b665c6830317dbcc89b2319e990becb
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-09-24 16:05:34 +0530

    Add CORS middleware to enable cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 // Import routes
 import healthCheckRoutes from "./routes/healthcheck.routes.js";
