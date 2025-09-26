7d2e75f2462eacf3022a93522f55590c30a1dc47
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-09-26 14:57:52 +0530

    Refactor authentication middleware to improve error handling and user role verification

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -27,7 +27,7 @@ export const verifyToken = asyncHandler(async (req, res, next) => {
   }
 
   req.user = user[0];
-  
+
   next();
 });
 
@@ -42,18 +42,23 @@ export const isAdmin = asyncHandler(async (req, res, next) => {
     token = token.slice(7).trim();
   }
 
-  const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  try {
+    const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-  if (!decodeToken.role || decodeToken.role !== "admin") {
-    throw new ApiError(401, "Unauthorized request");
+    if (!decodeToken.role || decodeToken.role !== "admin") {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const user = await getUserById(decodeToken.id);
+
+    if (!user) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    req.user = user;
+  } catch (error) {
+    throw new ApiError(401, "Unauthorized request")
   }
 
-  const user = await getUserById(decodeToken.id);
-
-  if (!user) {
-    throw new ApiError(401, "Unauthorized request");
-  }
-
-  req.user = user;
   next();
 });
